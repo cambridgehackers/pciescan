6008509c932603ad699ffad3c4f4246561db04f7
John Ankcorn <jca@alum.mit.edu>
2014-05-23 15:57:02 -0400

    removed 'sudo' from scripts.  On Centos, sudo is not allowed w/o a tty (just running the command over ssh)

--- pcieremove.sh
+++ pcieremove.sh
@@ -1,4 +1,4 @@
 #
-sudo rmmod pcieportal
-sudo sh -c "echo 1 >/sys/bus/pci/devices/0000:01:00.0/remove"
-sudo sh -c "echo 1 >/sys/bus/pci/rescan"
+rmmod pcieportal
+sh -c "echo 1 >/sys/bus/pci/devices/0000:01:00.0/remove"
+sh -c "echo 1 >/sys/bus/pci/rescan"
